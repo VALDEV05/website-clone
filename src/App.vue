81d1ba2309b14fa356f95b79a41e1d31e0a40e03
VALDEV05 <corda.valerio05@gmail.com>
2022-06-12 15:50:36 +0200

    Initial commit, install Vue and create project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header id="site-header">
+    header  
+  </header>
+  <!-- /#site-header -->
+   <main id="site-main">
+    main  
+  </main>
+  <!-- /#site-main -->
+   <footer id="site-footer">
+    footer  
+  </footer>
+  <!-- /#site-footer -->
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+/*#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
