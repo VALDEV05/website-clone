9ed489f9e864878fb26a94b8105de05ea246147f
VALDEV05 <corda.valerio05@gmail.com>
2022-06-17 18:09:47 +0200

    created a sub-component menu

--- src/components/HeaderAnimation.vue
+++ src/components/HeaderAnimation.vue
@@ -1,13 +1,19 @@
 <template>
-Header Animation
+  <div id="main-header">
+    <HeaderMenu></HeaderMenu>
+    <!-- /#header-menu -->
+  </div>
+  <!-- /#main-header -->
 </template>
 
 <script>
+import HeaderMenu from "./MenuHeader.vue";
 export default {
-
-}
+  components: {
+    HeaderMenu,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/MenuHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="header-menu">
+        <ul>
+            <li>Home</li>
+            <li>Chi Siamo</li>
+            <li>Contatti</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
