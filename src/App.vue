535f8a5efdd4146d2773b9ddea93dfb6534fedc2
VALDEV05 <corda.valerio05@gmail.com>
2022-06-19 23:13:47 +0200

    add favicon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
   <SiteHeader></SiteHeader>
   <!-- /#site-header -->
-   <main id="site-main">
-    <div class="btn btn-primary">main</div>
-  </main>
   <!-- /#site-main -->
-   <footer id="site-footer">
-    footer  
-  </footer>
   <!-- /#site-footer -->
 </template>
 
