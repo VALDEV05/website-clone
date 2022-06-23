151fda632c148660831bc14ad32cc50ded2ce6c8
VALDEV05 <corda.valerio05@gmail.com>
2022-06-23 23:56:44 +0200

    lavorando a chi siamo

--- src/components/ChiSiamo.vue
+++ src/components/ChiSiamo.vue
@@ -1,15 +1,47 @@
 <template>
-    <div id="chi-siamo">
-    
+  <div id="chi-siamo">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-5">AAAA</div>
+        <div class="col-md-7">
+          <div class="container-text-wrap">
+            <div class="container-text">
+              Chi Siamo
+              <h3 class="text-uppercase">HAREM STUDIO</h3>
+              <p>
+                Harem Studio nasce dalla passione e dal desiderio di creare un
+                luogo di condivisione per musicisti, cantanti, produttori e
+                videomakers con l'obiettivo di offrire servizi di qualità
+                durante tutte le fasi della realizzazione di un progetto.
+                Dedicato alla produzione e alla registrazione, offre tra gli
+                altri servizi: Mix, Master, Produzione di basi musicali,
+                distribuzione digitale, dj set ed eventi Live.
+              </p>
+              <p>
+                Lo studio è dotato di una sala di ripresa e una regia con tutte
+                le comodità per rilassarsi, free wi-fi, e aria condizionata. Le
+                competenze acquisite nel corso degli anni e la particolare
+                attenzione rivolta alla riuscita di ogni progetto fanno di
+                questa realtà un punto di riferimento per tutti coloro che sono
+                alla ricerca di un posto dove poter dar vita alla propria
+                musica.
+              </p>
+              <div class="button-wrap-contattaci">
+                <a href="#" class="btn btn-harem-outline text-uppercase"
+                  >Contattaci</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
