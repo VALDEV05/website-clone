193912a40b17a311595decf2558417e6315af36a
VALDEV05 <corda.valerio05@gmail.com>
2022-06-29 23:11:54 +0200

    Finita parte chi siamo

--- src/components/ChiSiamo.vue
+++ src/components/ChiSiamo.vue
@@ -3,41 +3,45 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-6">
-            <div class="container-sx-wrap">
-                <div class="content-text">
-                    <h1 class="text-light">PASSIONE</h1>
-                </div>
+          <div class="container-sx-wrap">
+            <div class="content-text">
+              <h1 class="text-light">PASSIONE</h1>
             </div>
+          </div>
         </div>
         <div class="col-md-6">
           <div class="container-text-wrap">
+            <!-- <span class="line"></span> -->
             <div class="container-text">
-              Chi Siamo
+              <h5>Chi siamo</h5>
               <h3 class="text-uppercase">HAREM STUDIO</h3>
-              <p>
-                Harem Studio nasce dalla passione e dal desiderio di creare un
-                luogo di condivisione per musicisti, cantanti, produttori e
-                videomakers con l'obiettivo di offrire servizi di qualità
-                durante tutte le fasi della realizzazione di un progetto.
-                Dedicato alla produzione e alla registrazione, offre tra gli
-                altri servizi: Mix, Master, Produzione di basi musicali,
-                distribuzione digitale, dj set ed eventi Live.
-              </p>
-              <p>
-                Lo studio è dotato di una sala di ripresa e una regia con tutte
-                le comodità per rilassarsi, free wi-fi, e aria condizionata. Le
-                competenze acquisite nel corso degli anni e la particolare
-                attenzione rivolta alla riuscita di ogni progetto fanno di
-                questa realtà un punto di riferimento per tutti coloro che sono
-                alla ricerca di un posto dove poter dar vita alla propria
-                musica.
-              </p>
+              <div class="description">
+                <p>
+                  Harem Studio nasce dalla passione e dal desiderio di creare un
+                  luogo di condivisione per musicisti, cantanti, produttori e
+                  videomakers con l'obiettivo di offrire servizi di qualità
+                  durante tutte le fasi della realizzazione di un progetto.
+                  Dedicato alla produzione e alla registrazione, offre tra gli
+                  altri servizi: Mix, Master, Produzione di basi musicali,
+                  distribuzione digitale, dj set ed eventi Live.
+                </p>
+                <p>
+                  Lo studio è dotato di una sala di ripresa e una regia con
+                  tutte le comodità per rilassarsi, free wi-fi, e aria
+                  condizionata. Le competenze acquisite nel corso degli anni e
+                  la particolare attenzione rivolta alla riuscita di ogni
+                  progetto fanno di questa realtà un punto di riferimento per
+                  tutti coloro che sono alla ricerca di un posto dove poter dar
+                  vita alla propria musica.
+                </p>
+              </div>
               <div class="button-wrap-contattaci">
                 <a href="#" class="btn btn-harem-outline text-uppercase"
                   >Contattaci</a
                 >
               </div>
             </div>
+            <!-- <span class="line"></span> -->
           </div>
         </div>
       </div>
